87d45323d7321cebb1bf9430b2404e8d99ea3de1
Razi Benvidi <raziyehes859@gmail.com>
2024-08-19 17:59:42 -0400

    UI for the user experience added

--- src/components/resume/Stepper.tsx
+++ src/components/resume/Stepper.tsx
@@ -11,6 +11,7 @@ import {
 import { Step, type StepItem, Stepper } from "@/components/ui/stepper";
 import StepperFooter from "./StepperFooter";
 import BasicUserDetailForm from "./BasicUserDetailForm";
+import ExperienceLayout from "./ExperienceLayout";
 
 const steps = [
   { label: "User Details", icon: User },
@@ -36,6 +37,7 @@ export default function StepperExamples() {
             <Step key={stepProps.label} {...stepProps}>
               <div className="p-6 border rounded-md ">
                 {index == 0 && <BasicUserDetailForm />}
+                {index == 1 && <ExperienceLayout />}
               </div>
             </Step>
           );
